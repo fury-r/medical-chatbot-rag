66789be96bcce1cebddae2077a1ed0b5e9ecbd2d
fury-r <rajeev.dessai11@gmail.com>
2025-05-25 17:06:44 +0530

    🚨 fix-lint

--- src/model/medical_rag.py
+++ src/model/medical_rag.py
@@ -49,16 +49,16 @@ class MedicalRAG:
 
         memory = (
             ConversationBufferWindowMemory(
-                memory_key="chat_history", 
-                k=k, 
-                return_messages=True, 
-                output_key="answer"
+                    memory_key="chat_history", 
+                    k=k, 
+                    return_messages=True, 
+                    output_key="answer"
                 )
             if k > 0
             else ConversationBufferMemory(
-                memory_key="chat_history", 
-                return_messages=True, 
-                output_key="answer"
+                    memory_key="chat_history", 
+                    return_messages=True, 
+                    output_key="answer"
                 )
         )
 
